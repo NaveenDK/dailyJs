5f0f7cb77dc8bc65a139d3f9cc42fc7127051d8e
naveendk <naveenkolambage@gmail.com>
2019-07-06 06:56:04 +1200

    ternary operator-definition

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-let person = {
-  name: "Mosh",
-  age: 30
-};
-//Dot notation
-person.age = "John";
-//Bracket notation
-let selection = "name";
-person[selection] = "Mary";
-console.log(person.name);
+//if a customer has more than 100 points
+//they are a gold customer
+//otherwise they are a silver customer
+
+let points = 110;
+let type = points > 100 ? "gold" : "silver";
+
+console.log(type);
