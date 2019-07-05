465c1417395bbd77e53532aaa8b7ef95dd54d58c
naveendk <naveenkolambage@gmail.com>
2019-07-06 05:01:11 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = "naveen";
+console.log(name);
+
+//cannot be a reserved keyword
+//should be meaningful
+//Cannot start with a number
+//Cannot contain a space or hiphen (-)
+
+let firstName = "Naveen";
+let lastName = "Kolambage";
