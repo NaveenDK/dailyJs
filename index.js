e0959c9aa14ced6fb87de8261c22b8d6e45f6620
naveendk <naveenkolambage@gmail.com>
2019-07-07 04:43:00 +1200

    answer to question-swapping 2 variables

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ let a = "red";
 let b = "blue";
 
 //write code to swap the variables a console outputs b and vice versa
+let c = a;
+a = b;
+b = c;
+
+//end code
+console.log("The values : ");
+
 console.log(a);
 console.log(b);
