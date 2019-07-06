0ec8ae1d2f74f2b13245207812e8572a69c62042
naveendk <naveenkolambage@gmail.com>
2019-07-07 04:56:25 +1200

    answer ifelse

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //Using if else
 //Hour, if hour is bw 6am and 12pm: Good morning!
+//If it is between 12pm and 6pm : Good afternoon!
+//Otherwise : Good evening!
 //Basic structure
-if (condition) {
-  statement;
-} else if (anotherCondition) {
-  statement;
-} else if (yetAnotherCondition) {
-  statement;
-} else statement;
+let hour = 19;
+if (hour >= 6 && hour < 12) {
+  //  statement;
+  console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good afternoon");
+}
+// statement;
+else console.log("Good evening!");
