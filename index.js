c35ddf98e687a9d300c59679039e0dfd11ed21f9
naveendk <naveenkolambage@gmail.com>
2019-07-08 04:43:50 +1200

    Question - Complete the function isLandscape, which returns true when width > height

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-//Exercise 1- Max of Two Numbers
-//Write a function that takes two numbers and returns the maximum of the two
+//Exercise 2
+//Complete the function isLandscape(width,height)
 
-function checkMax(a, b) {
-  if (a == b) {
-    console.log("both same");
-  } else if (a > b) {
-    console.log(a + " is the max");
-  } else if (b > a) {
-    console.log(b + " is the max");
-  }
-}
-
-checkMax(1, 3);
-checkMax(4, 2);
-checkMax(5, 5);
-
-//recommended answer
-function max(a, b) {
-  return a > b ? a : b;
+function isLandscape(width, height) {
+  //
 }
