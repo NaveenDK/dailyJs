2ff78d08484e629e3ae4a4d7bc3f54be598dd762
naveendk <naveenkolambage@gmail.com>
2019-07-08 04:54:15 +1200

    answer-exercise-2

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//Exercise 2
-//Complete the function isLandscape(width,height)
+//Exercise 2 Landscape or portrate
+
+// function isLandscape(width, height) {
+//   return width > height ? true : false;
+// }
+
+// console.log(isLandscape(2, 5));
+
+//recommended answer
 
 function isLandscape(width, height) {
-  //
+  return width > height;
 }
+
+console.log(isLandscape(20, 5));
