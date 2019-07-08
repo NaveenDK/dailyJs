4b91babfff53e89c0ee0416fe252ce238c374dc4
naveendk <naveenkolambage@gmail.com>
2019-07-09 09:04:08 +1200

    Answer-Implement a function to say its Odd/Even

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-//Exercise 4
-checkSpeed(130);
-
-// function checkSpeed(speed) {
-//   if (speed > 75) {
-//     if ((speed - 70) / 5 >= 12) return "License Suspended";
-//     else return Math.floor((speed - 70) / 5) + " Demerit Point/s";
-//   }
-//   return "Ok";
-// }
-
-//recommended answer
-
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-
-  if (speed < speedLimit + kmPerPoint) console.log("Ok");
-  else {
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points >= 12) console.log("License suspended");
-    else console.log("Points", points);
-  }
-}
-//Indentation issue, - use return; and remove else
-function checkSpeedIndentationfixed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-
-  if (speed < speedLimit + kmPerPoint) {
-    console.log("Ok");
-    return;
+//Exercise 5
+//write a function that will take a number and say ODD or EVEN printing out infront of the number
+showNumbers(3);
+function showNumbers(limit) {
+  if (limit % 2 == 0) {
+    console.log(limit + " Even");
   }
 
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log("License suspended");
-  else console.log("Points", points);
+  if (limit % 2 != 0) {
+    console.log(limit + " Odd");
+  }
 }
