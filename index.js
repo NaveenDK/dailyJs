e026fa8543183e8828be8af2ad550afe81cf4a45
naveendk <naveenkolambage@gmail.com>
2019-07-09 09:05:59 +1200

    Answer-Implement a function to say its Odd/Even

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Exercise 5
 //write a function that will take a number and say ODD or EVEN printing out infront of the number
 showNumbers(3);
+
 function showNumbers(limit) {
   if (limit % 2 == 0) {
     console.log(limit + " Even");
