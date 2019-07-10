eded315bea711a0774cd1a8af2946c2bed78a472
naveendk <naveenkolambage@gmail.com>
2019-07-11 05:03:31 +1200

    Answer 6 - check truthy values of an array

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-//Exercise 5
-//write a function that will take a number and say ODD or EVEN printing out infront of the number
-showNumbers(10);
+//Exercise 6
+//Count truthy values of  an array
 
-// function showNumbers(limit) {
-//   for (i = 1; i <= limit; i++) {
-//     if (i % 2 == 0) {
-//       console.log(i + " Even");
-//     }
-
-//     if (i % 2 != 0) {
-//       console.log(i + " Odd");
-//     }
-//   }
-// }
-
-function showNumbers(limit) {
-  for (let i = 0; i <= limit; i++) {
-    const message = i % 2 === 0 ? "EVEN" : "ODD";
-    console.log(i, message);
-  }
+//All Falsy
+//undefined
+//null
+//' '
+//false
+//0
+//NaN
+// for example const array = [0,null,undefined,'',1,2,3]
+//countTruthy in the above case will return 3
+const array = [0, null, undefined, "", 1, 2, 4, 4, 3];
+function countTruthy(array) {
+  var trueCount = 0;
+  for (let value of array) if (value) trueCount++;
+  return trueCount;
 }
+
+console.log(countTruthy(array));
