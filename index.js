7e56ef90a72d9be5e18986bfdcf7647a67319386
naveendk <naveenkolambage@gmail.com>
2019-07-11 04:39:36 +1200

    Answer 5

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 //Exercise 5
 //write a function that will take a number and say ODD or EVEN printing out infront of the number
-showNumbers(3);
+showNumbers(10);
+
+// function showNumbers(limit) {
+//   for (i = 1; i <= limit; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i + " Even");
+//     }
+
+//     if (i % 2 != 0) {
+//       console.log(i + " Odd");
+//     }
+//   }
+// }
 
 function showNumbers(limit) {
-  if (limit % 2 == 0) {
-    console.log(limit + " Even");
-  }
-
-  if (limit % 2 != 0) {
-    console.log(limit + " Odd");
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
   }
 }
