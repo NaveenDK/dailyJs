0ae425bc3e0b1de6bdd63c7bac8f4f9e0703ecd3
naveendk <naveenkolambage@gmail.com>
2019-07-11 06:24:15 +1200

    Answer 9- Calculate grade, seperation of concerns

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-//Exercise 8
-// Sum of multiples of 3 and 5
+//Exercise 9
+//
+const marks = [80, 80, 50];
+//Calculate average and give the grade based on the citeria
+//1-59 :F
+//60-69  : D
+//70-79 : C
+//80-89 : B
+//90-100 : A
 
-function sum3n5(limit) {
-  let sum = 0;
-  for (let i = 0; i <= limit; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum = sum + i;
-    }
-  }
-  return sum;
+console.log(calculateGrade(marks));
+
+// function calculateGrade(marks) {
+//   let sum = 0;
+//   for (i = 0; i < marks.length; i++) {
+//     sum = sum + marks[i];
+//   }
+//   console.log(sum);
+//   console.log(marks.length);
+//   let average = sum / marks.length;
+//   if (average < 60) {
+//     return "F";
+//   }
+//   if (average < 70 && average >= 60) {
+//     return "D";
+//   }
+//   if (average < 80 && average >= 70) {
+//     return "C";
+//   }
+//   if (average < 90 && average >= 80) {
+//     return "B";
+//   }
+//   if (average < 100 && average >= 90) {
+//     return "A";
+//   }
+// }
+
+function calculateGrade(marks) {
+  const average = calculateAverage(marks);
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
 }
 
-console.log(sum3n5(10));
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of array) sum += value;
+
+  return sum / array.length;
+}
