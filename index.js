a8b7487aa5c61465cf6f927559a712504d1c21ab
naveendk <naveenkolambage@gmail.com>
2019-07-12 05:31:17 +1200

    showPrimes function

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
-//Exercise 9
-//
-const marks = [80, 80, 50];
-//Calculate average and give the grade based on the citeria
-//1-59 :F
-//60-69  : D
-//70-79 : C
-//80-89 : B
-//90-100 : A
-
-console.log(calculateGrade(marks));
-
-// function calculateGrade(marks) {
-//   let sum = 0;
-//   for (i = 0; i < marks.length; i++) {
-//     sum = sum + marks[i];
-//   }
-//   console.log(sum);
-//   console.log(marks.length);
-//   let average = sum / marks.length;
-//   if (average < 60) {
-//     return "F";
-//   }
-//   if (average < 70 && average >= 60) {
-//     return "D";
-//   }
-//   if (average < 80 && average >= 70) {
-//     return "C";
-//   }
-//   if (average < 90 && average >= 80) {
-//     return "B";
-//   }
-//   if (average < 100 && average >= 90) {
-//     return "A";
+//Exercise 11
+//Print all prime numbers in the limit
+//Basic Answer
+// function showPrimes(limit) {
+//   for (let number = 2; number <= limit; number++) {
+//     let isPrime = true;
+//     for (let factor = 2; factor < number; factor++) {
+//       if (number % factor === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     if (isPrime) console.log(number);
 //   }
 // }
+showPrimes(10);
+//recommended Answer, functions with single responsibility
 
-function calculateGrade(marks) {
-  const average = calculateAverage(marks);
-  if (average < 60) return "F";
-  if (average < 70) return "D";
-  if (average < 80) return "C";
-  if (average < 90) return "B";
-  return "A";
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++) {
+    isPrime(number);
+    console.log(number);
+  }
 }
 
-function calculateAverage(array) {
-  let sum = 0;
-  for (let value of array) sum += value;
-
-  return sum / array.length;
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++) {
+    if (number % factor == 0) return false;
+  }
 }
