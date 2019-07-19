d2452e3ab47f100478361ce80537fb1c6e6693d5
NaveenDK <naveenkolambage@gmail.com>
2019-07-20 05:16:10 +1200

    S2 Ex1 - Answer

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 //Section 2 Exercise 1
 //Create an address object with, street city,zipCode
 //showAddress(address) , object that displays all properties, with values
+
+let address ={
+  street:"a",
+  city :"b",
+  zipCode: "c"
+};
+
+function showAddress(address){
+  for (let key in address)
+    console.log(key,address[key]);
+}
+console.log(address);
