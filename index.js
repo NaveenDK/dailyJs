8e04cab70e50eda62425062e5e3b4bb1aa5d94e7
NaveenDK <naveenkolambage@gmail.com>
2019-07-20 10:47:50 +1200

    create constructor function to initialize draft post

--- index.js
+++ index.js
@@ -7,14 +7,16 @@
 //(author,body) properties for comment
 //isLive
 
-let post ={
-  title: 'title1',
-  body: 'test the body',
-  author:"nave",
-  views: 2,
-  comments:[
-    {author:'a',body:'b'},
-    {author:'c', body:'d'}
-  ],
-  isLive:false,
+let post = new Post ('a','b','c');
+
+console.log(post);
+
+function Post(title,body,author){
+this.title = title;
+this.body = body;
+this.author = author;
+this.views=0;
+this.comments= [];
+this.isLive=false;
 }
+
