50d5a8f9022242f8e2da5e6e5114fa400040b4d3
NaveenDK <naveenkolambage@gmail.com>
2019-07-20 10:11:51 +1200

    S2 Ex4 - Answer

--- index.js
+++ index.js
@@ -6,3 +6,15 @@
 //comments
 //(author,body) properties for comment
 //isLive
+
+let post ={
+  title: 'title1',
+  body: 'test the body',
+  author:"nave",
+  views: 2,
+  comments:[
+    {author:'a',body:'b'},
+    {author:'c', body:'d'}
+  ],
+  isLive:false,
+}
