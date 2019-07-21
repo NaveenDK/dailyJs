55d655b26d2abc92908fbe706684cb0c785ebd5f
NaveenDK <naveenkolambage@gmail.com>
2019-07-21 14:31:23 +1200

    find a max of an array using reduce

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const numbers = [1,2,3,4,1];
+const numbers = [1,2];
 
-const count = countOccurences(numbers,3);
+const max = getMax(numbers);
 
-// function countOccurrences(array,searchElement){
-//     let i=0;
-//     for(let element of array){
+console.log(max);
 
-//         if (element==searchElement){
-//             i++;
-//         }
-//     }
-//     return i;
-// }
-
-function countOccurences(array,searchElement){
-   // const reducer = (accumulator,currentValue) => accumulator+ currentValue
-  return  array.reduce(function(acc,current){
- //  console.log(acc);
-       let occurences = searchElement=== current? 1:0;
- //  console.log(count);
-      return occurences +acc;   
-  },0);
+function getMax(array){
+    // let max= array[0];
+    // for (i=0;i<array.length;i++){
+    //     if(max<array[i]){
+    //         max=array[i]
+    //     }
+    // }
+    // return max;
+    if(array.length === 0) return undefined;
+    
+    return array.reduce((accumulator, current)=>{
+    (accumulator<current) ? accumulator=current: accumulator;
+    },0);
+    
 }
-
-console.log(count);
